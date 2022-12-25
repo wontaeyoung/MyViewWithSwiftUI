960de510dd08263a09ae71a3f42430e3f9fce68e
Taeyoung Won <hexter1994@gmail.com>
2022-12-25 18:14:46 +0900

    View : MyScrollView 생성

---
+++ MyViewWithSwiftUI/View/ScrollView/MyScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  MyScrollView.swift
+//  MyViewWithSwiftUI
+//
+//  Created by 원태영 on 2022/12/25.
+//
+
+import SwiftUI
+
+struct MyScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyScrollView()
+    }
+}
