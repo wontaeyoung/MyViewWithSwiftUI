b6001c15483119deafce891f4289d8ff2c7109f3
Taeyoung Won <hexter1994@gmail.com>
2022-12-25 17:52:18 +0900

    Project Init

---
+++ MyViewWithSwiftUI/MyViewWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyViewWithSwiftUIApp.swift
+//  MyViewWithSwiftUI
+//
+//  Created by 원태영 on 2022/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MyViewWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
