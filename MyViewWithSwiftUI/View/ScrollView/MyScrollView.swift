be713788832f1a99fa20ff93ee49ce80397f1d5f
Taeyoung Won <hexter1994@gmail.com>
2022-12-29 22:12:31 +0900

    Feat: ScrollView와 Reader 코드 추가

--- MyViewWithSwiftUI/View/ScrollView/MyScrollView.swift
+++ MyViewWithSwiftUI/View/ScrollView/MyScrollView.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct MyScrollView: View {
+    
+    let IDList : [Int] = [0,1,2,3]
+    
+    @ObservedObject var scrollViewModel : ScrollViewModel = ScrollViewModel(
+        scrollModels: [scrollModel1, scrollModel2, scrollModel3, scrollModel4]
+    )
+    @State private var currentID : Int = 0
+    @State private var isAgreeDone : Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // MARK: -ScrollView : proxy로 스크롤을 조절할 수 있는 ScrollView
+        ScrollViewReader { proxy in
+            ScrollView {
+                VStack(alignment : .leading) {
+                    ForEach(scrollViewModel.scrollModels.indices) { index in
+                        Text(scrollViewModel.scrollModels[index].title)
+                            .font(.largeTitle.bold())
+                            .padding(.top, index == 0 ? 0 : 50)
+                            .padding(.bottom, 20)
+                            .id(IDList[index])
+                        
+                        Text(scrollViewModel.scrollModels[index].content)
+                            .font(.body)
+                            .padding(.bottom, 80)
+                        
+                    }
+                    .padding(.horizontal, 20)
+                }
+                .padding(.top, 60)
+            }
+        }
     }
 }
+
+
+
 
 struct MyScrollView_Previews: PreviewProvider {
     static var previews: some View {
